ba57849a3fc6ee830289898ebec6266a5ba81317
Nicklas <nicklas.hermann@gmail.com>
2019-05-24 14:39:21 +0200

    Now with unit test on first and last name.
    Validation is also moved too seperate  methods

--- TestingFinalExam/Controllers/HomeController.cs
+++ TestingFinalExam/Controllers/HomeController.cs
@@ -24,79 +24,23 @@ namespace TestingFinalExam.Controllers
 		[HttpPost]
 		public IActionResult Login([Bind("firstname,lastname,phonenumber,age,guessScore")]UserModel user)
         {
-			string errormessage = "";
             
-			if(user.firstname.Length >= 20)
-			{
-				ViewBag.firstnameTrue = true;
-				errormessage = "Your firstname needs to be less then 30 charectors";            
-				ModelState.AddModelError("firstname", errormessage);
-			}
-			else if (user.firstname.Any(char.IsDigit)==true)
-            {
-                ViewBag.firstnameTrue = true;
-                errormessage = "Your name can not contain digits";
-                ModelState.AddModelError("firstname", errormessage);
-            }
-			else if (user.firstname.Any(char.IsSymbol) == true)
-            {
-                ViewBag.firstnameTrue = true;
-                errormessage = "Your name can not contain symbols";
-                ModelState.AddModelError("firstname", errormessage);
-            }
+			string errormessageFirstname = validateFirstname(user.firstname);
+            ModelState.AddModelError("firstname", errormessageFirstname);
 
-			else if (user.lastname.Length >= 20)
-            {
-                ViewBag.lastnameTrue = true;
-                errormessage = "Your lastname needs to be less then 30 charectors";
-                ModelState.AddModelError("lastname", errormessage);
-            }
-			else if (user.lastname.Any(char.IsDigit) == true)
-            {
-                ViewBag.lastnameTrue = true;
-                errormessage = "Your lastname can not contain digits";
-                ModelState.AddModelError("lastname", errormessage);
-            }
-			else if (user.lastname.Any(char.IsSymbol) == true)
-            {
-                ViewBag.lastnameTrue = true;
-                errormessage = "Your name can not contain symbols";
-                ModelState.AddModelError("lastname", errormessage);
-            }
-			else if(user.phonenumber.ToString().Length !=8 && !user.phonenumber.ToString().Equals("0")){
-				ViewBag.phonenumberTrue = true;
-    				errormessage = "Phonenumber needs to be exactly 8 digits";
-                ModelState.AddModelError("phonenumber", errormessage);
-			}
-			else if (user.phonenumber.ToString().Any(char.IsLetter) == true || user.phonenumber.ToString().Any(char.IsSymbol) == true)
-            {
-                ViewBag.phonenumberTrue = true;
-                errormessage = "Phonenumber can not contain letters or symbols";
-                ModelState.AddModelError("phonenumber", errormessage);
-            }
-			else if (!user.age.ToString().Any(char.IsDigit)==true)
-            {
-                ViewBag.ageTrue = true;
-                errormessage = "Age must be numeric value";
-                ModelState.AddModelError("age", errormessage);
-            }
-			else if (user.age <5  || user.age > 12)
-            {
-                ViewBag.ageTrue = true;
-                errormessage = "To enter, you must between the age 5-12 to enter";
-                ModelState.AddModelError("age", errormessage);
-            }
-			else if(user.guessScore <-6 || user.guessScore >12)
-			{
-				ViewBag.guessScore = true;
-				errormessage = "Guess has to be between -6 and 12";
-                ModelState.AddModelError("guessScore", errormessage);
-			}
-			else{
-				errormessage = "";
-			}
+            string errormessageLastname = validateLastname(user.lastname);
+            ModelState.AddModelError("lastname", errormessageLastname);
 
-			if (errormessage == "")
+            string errormessagePhonenumber = validatePhonenumber(user.phonenumber);
+            ModelState.AddModelError("phonenumber", errormessagePhonenumber);
+
+            string errormessageAge = validateAge(user.age);
+            ModelState.AddModelError("age", errormessageAge);
+
+            string errormessageGuessScore = validateGuessScore(user.guessScore);
+            ModelState.AddModelError("guessScore", errormessageGuessScore);
+
+            if (errormessageFirstname == "" && errormessageLastname == "" && errormessagePhonenumber == "" && errormessageAge == "" && errormessageGuessScore == "")
 			{
                 MathLogicController mathLogicController = new MathLogicController();
                 mathLogicController.SetUser(user);
@@ -105,7 +49,123 @@ namespace TestingFinalExam.Controllers
 
 			return View();
         }
-       
+
+
+        public string validateFirstname(string firstname) {
+            string errormessage = "";
+
+            if (firstname.Length >= 20)
+            {
+                ViewBag.firstnameTrue = true;
+                errormessage = "Your firstname needs to be less then 20 characters";
+                //ModelState.AddModelError("firstname", errormessage);
+                return errormessage;
+            }
+            else if (firstname.Any(char.IsDigit) == true)
+            {
+                ViewBag.firstnameTrue = true;
+                errormessage = "Your name can not contain digits";
+                //ModelState.AddModelError("firstname", errormessage);
+                return errormessage;
+            }
+            else if (firstname.Any(char.IsSymbol) == true)
+            {
+                ViewBag.firstnameTrue = true;
+                errormessage = "Your name can not contain symbols";
+                //ModelState.AddModelError("firstname", errormessage);
+                return errormessage;
+            }
+
+            return errormessage;
+        }
+
+        public string validateLastname(string lastname)
+        {
+            string errormessage = "";
+
+            if (lastname.Length >= 20)
+            {
+                ViewBag.lastnameTrue = true;
+                errormessage = "Your lastname needs to be less then 20 characters";
+                //ModelState.AddModelError("lastname", errormessage);
+                return errormessage;
+            }
+            else if (lastname.Any(char.IsDigit) == true)
+            {
+                ViewBag.lastnameTrue = true;
+                errormessage = "Your lastname can not contain digits";
+                //ModelState.AddModelError("lastname", errormessage);
+                return errormessage;
+            }
+            else if (lastname.Any(char.IsSymbol) == true)
+            {
+                ViewBag.lastnameTrue = true;
+                errormessage = "Your lastname can not contain symbols";
+                //ModelState.AddModelError("lastname", errormessage);
+                return errormessage;
+            }
+
+            return errormessage;
+        }
+
+        public string validatePhonenumber(int phonenumber)
+        {
+            string errormessage = "";
+
+            if (phonenumber.ToString().Length != 8 && !phonenumber.ToString().Equals("0"))
+            {
+                ViewBag.phonenumberTrue = true;
+                errormessage = "Phonenumber needs to be exactly 8 digits";
+                //ModelState.AddModelError("phonenumber", errormessage);
+                return errormessage;
+            }
+            else if (phonenumber.ToString().Any(char.IsLetter) == true || phonenumber.ToString().Any(char.IsSymbol) == true)
+            {
+                ViewBag.phonenumberTrue = true;
+                errormessage = "Phonenumber can not contain letters or symbols";
+                //ModelState.AddModelError("phonenumber", errormessage);
+                return errormessage;
+            }
+
+            return errormessage;
+        }
+
+        public string validateAge(int age)
+        {
+            string errormessage = "";
+
+            if (!age.ToString().Any(char.IsDigit) == true)
+            {
+                ViewBag.ageTrue = true;
+                errormessage = "Age must be numeric value";
+                //ModelState.AddModelError("age", errormessage);
+                return errormessage;
+            }
+            else if (age < 5 || age > 12)
+            {
+                ViewBag.ageTrue = true;
+                errormessage = "To enter, you must between the age 5-12 to enter";
+                //ModelState.AddModelError("age", errormessage);
+                return errormessage;
+            }
+
+            return errormessage;
+        }
+
+        public string validateGuessScore(int guessScore)
+        {
+            string errormessage = "";
+
+            if (guessScore < -6 || guessScore > 12)
+            {
+                ViewBag.guessScore = true;
+                errormessage = "Guess has to be between -6 and 12";
+                //ModelState.AddModelError("guessScore", errormessage);
+                return errormessage;
+            }
+
+            return errormessage;
+        }
 
         public IActionResult Error()
         {
